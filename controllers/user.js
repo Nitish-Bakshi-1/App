a6ade78ca749861eaf6c8fb436f293c654987a72
Nitish-Bakshi-1 <nitishbakshi05@gmail.com>
2024-12-06 22:38:14 +0530

    signin and signout logic done

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,8 @@
 import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signUp = async (req, res) => {
   try {
@@ -17,4 +21,35 @@ export const signUp = async (req, res) => {
   } catch (e) {
     console.log(e);
   }
+};
+export const signIn = async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    const currentUser = await User.findOne({
+      password,
+      email,
+    });
+    if (currentUser) {
+      const token = jwt.sign({ id: currentUser._id }, "displined");
+      res
+        .cookie("token", token, {
+          maxAge: 10 * 60 * 1000,
+          httpOnly: true,
+        })
+        .json({
+          message: "signed in ",
+        });
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+export const signOut = async (req, res) => {
+  res
+    .cookie("token", "", {
+      httpOnly: true,
+    })
+    .json({
+      message: "signed out",
+    });
 };
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 // importing stuff
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDb } from "./data/db.js";
-const app = express();
-
 import { signUp } from "./controllers/user.js";
 import { signIn } from "./controllers/user.js";
+import { signOut } from "./controllers/user.js";
+
+const app = express();
 
 connectDb();
 
 dotenv.config();
 
+// using middlewares
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", signUp);
 app.post("/signin", signIn);
+app.get("/signout", signOut);
 
 app.listen(3000, () => {
   console.log("server started");
